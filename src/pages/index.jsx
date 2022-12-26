3be0c417c895d28864ebbf1026d980ace67eaeb0
nv21063-hasan-ali <NV21063@nvtc.edu.bh>
2022-12-26 10:22:33 +0300

    Spencer

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -264,10 +264,7 @@ export default function Home({ articles }) {
           Cloud Architect
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
-            I’m Spencer, a software designer and entrepreneur based in New York
-            City. I’m the founder and CEO of Planetaria, where we develop
-            technologies that empower regular people to explore space on their
-            own terms.
+            I’m cloud architect student
           </p>
           <div className="mt-6 flex gap-6">
             <SocialLink
